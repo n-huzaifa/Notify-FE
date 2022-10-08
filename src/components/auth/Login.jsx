0e7451e421d986a7ca5c4407c2f46b898205836e
n-huzaifa <noorhuzaifa11@gmail.com>
2022-10-09 01:15:13 +0500

    login & signup API Integration

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Footer from "../Footer";
+import axios from "axios";
 
-function login() {
+const serverURI = "http://localhost:8000/api/auth/login";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const loginData = { email, password };
+    axios
+      .post(serverURI, loginData)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div className='flex flex-col items-center justify-center h-screen font-poppins text-white'>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className=' w-96 p-6'>
           <div className='flex item-center justify-center mb-8'>
             <span className=' text-2xl font-extralight'>
@@ -19,6 +35,7 @@ function login() {
             type='email'
             placeholder='foo@bar.com'
             className='input w-full max-w-xs'
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor='login-password' className='label'>
             Password
@@ -28,6 +45,7 @@ function login() {
             type='password'
             placeholder='foobar123'
             className='input w-full max-w-xs'
+            onChange={(e) => setPassword(e.target.value)}
           />
           <div className='flex flex-col justify-center items-center space-y-2'>
             <input
@@ -37,10 +55,10 @@ function login() {
             />
             <span className='text-sm font-light text-center'>
               Don't have an account ?
-              <a href='https://google.com' className='text-primary'>
+              <Link to='/signup' className='text-primary'>
                 {" "}
                 Signup
-              </a>
+              </Link>
             </span>
           </div>
         </div>
@@ -50,4 +68,4 @@ function login() {
   );
 }
 
-export default login;
+export default Login;
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Footer from "../Footer";
+import axios from "axios";
+
+const serverURI = "http://localhost:8000/api/auth/signup";
 
 function Signup() {
+  const [signupForm, setSignupForm] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    setSignupForm({ ...signupForm, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(signupForm);
+    axios
+      .post(serverURI, signupForm)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className='flex flex-col items-center justify-center h-screen font-poppins text-white'>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className=' w-96 p-6'>
           <div className='flex item-center justify-center mb-8'>
             <span className=' text-2xl font-extralight'>
@@ -13,25 +37,29 @@ function Signup() {
           </div>
           <div className='flex space-x-2'>
             <div className='flex flex-col'>
-              <label htmlFor='firstname' className='label'>
+              <label htmlFor='first_name' className='label'>
                 Firstname
               </label>
               <input
-                id='firstname'
+                id='first_name'
+                name='first_name'
                 type='text'
                 placeholder='John'
                 className='input w-full max-w-xs'
+                onChange={changeHandler}
               />
             </div>
             <div className='flex flex-col'>
-              <label htmlFor='lastname' className='label'>
+              <label htmlFor='last_name' className='label'>
                 Lastname
               </label>
               <input
-                id='lastname'
+                id='last_name'
+                name='last_name'
                 type='text'
                 placeholder='Doe'
                 className='input w-full '
+                onChange={changeHandler}
               />
             </div>
           </div>
@@ -40,18 +68,22 @@ function Signup() {
           </label>
           <input
             id='email'
+            name='email'
             type='email'
             placeholder='JohnDoe@example.com'
             className='input w-full '
+            onChange={changeHandler}
           />
           <label htmlFor='password' className='label'>
             Password
           </label>
           <input
             id='password'
+            name='password'
             type='password'
             placeholder='foobar123'
             className='input w-full'
+            onChange={changeHandler}
           />
           <div className='flex flex-col justify-center items-center space-y-2'>
             <input
@@ -61,10 +93,10 @@ function Signup() {
             />
             <span className='text-sm font-light text-center'>
               Already have an account ?
-              <a href='https://google.com' className='text-primary'>
+              <Link to='/login' className='text-primary'>
                 {" "}
                 Login
-              </a>
+              </Link>
             </span>
           </div>
         </div>
