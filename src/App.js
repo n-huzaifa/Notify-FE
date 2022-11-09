b7e10074e1ff7d0254ebb82d2de036e052c942b4
n-huzaifa <noorhuzaifa11@gmail.com>
2022-11-09 23:32:12 +0500

    Context Integrtion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
 import Home from "./pages/Home";
 import Note from "./pages/Note";
+import React, { useState } from "react";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import PageNotFound from "./pages/PageNotFound";
 import { Route, Routes } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const [login, setLogin] = useState(false);
   return (
     <div className='bg-secondary h-screen'>
-      <Routes>
-        <Route path='auth' element={<Home />} />
-        <Route path='auth/login' element={<Login />} />
-        <Route path='auth/signup' element={<Signup />} />
-        <Route path='/' element={<Note />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
+      <AuthContext.Provider value={{ login, setLogin }}>
+        <Routes>
+          <Route path='auth' element={<Home />} />
+          <Route path='auth/login' element={<Login />} />
+          <Route path='auth/signup' element={<Signup />} />
+          <Route path='/' element={<Note />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </AuthContext.Provider>
     </div>
   );
 }
